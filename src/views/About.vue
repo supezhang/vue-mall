bc6eae0f69287d742d5af65c1373392f049b8060
chaozhang <m15034600214@163.com>
2021-05-30 20:21:47 +0800

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>2342</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    
+  }
+}
+</script>
